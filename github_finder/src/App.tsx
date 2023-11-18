b857e9c2c9a82d3b41db1d9d34088eb6f993ef44
Allanpt <jatallantavares@gmail.com>
2023-11-18 11:56:18 -0300

    Find user complete

--- github_finder/src/App.tsx
+++ github_finder/src/App.tsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router-dom"
 
+import classes from "./App.module.css"
+
 function App() {
 
   return (
-    <div>
+    <div className={classes.app}>
         <h1>Github Finder</h1>
         <Outlet/>
     </div>
--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -2,16 +2,31 @@ import { UserProps } from '../types/user'
 import { useState } from 'react'
 
 import Search from '../components/Search'
+import User from '../components/User'
+import Erro from '../components/Erro'
 
 function Home() {
 
     const [user, setUser] = useState<UserProps | null>(null)
-    
+    const [erro, setErro] = useState(false)
+
     const loadUser = async (username:string) => {
         
         const res = await fetch(`https://api.github.com/users/${username}`)
 
         const data = await res.json()
+
+        if(username === ""){
+          setUser(null)
+          setErro(false)
+          return
+        }
+        
+        if(res.status === 404){
+          setUser(null)
+          setErro(true)
+          return
+        }
 
         const { avatar_url, login, location, followers, following} = data
 
@@ -22,13 +37,16 @@ function Home() {
             followers,
             following
         }
+        console.log(userData)
+        setErro(false)
         setUser(userData)
     }
 
   return (
     <div>
         <Search loadUser={loadUser}/>
-        {user && <p>{user.login}</p>}
+        {erro && <Erro/>}
+        {user && <User {...user}/>}
     </div>
   )
 }
